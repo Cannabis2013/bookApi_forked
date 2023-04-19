7b4bb6d952013b12a781c82bafbe0678cdb32f47
mura0717 <mura0717@stud.kea.dk>
2023-04-19 14:31:00 +0200

    open aichoices dto added

--- src/main/java/com/example/googlebooksapi/dtos/openai/OpenAiChoices.java
+++ src/main/java/com/example/googlebooksapi/dtos/openai/OpenAiChoices.java
@@ -1,4 +1,13 @@
 package com.example.googlebooksapi.dtos.openai;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class OpenAiChoices {
+
+    private String text;
 }
